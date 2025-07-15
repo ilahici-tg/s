1a453704d22ed1b357c828c25baefb5371696a9a
Yusuf Balcı <109670034+ilahici-tg@users.noreply.github.com>
2025-07-15 22:50:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-// Menü açma ve kapama
-const menuIcon = document.getElementById("menu-icon");
-const navLinks = document.querySelector(".nav-links");
+function rain(){
+  let cloud = document.querySelector('.cloud');
+  let e = document.createElement('div');
+  let left = Math.floor(Math.random() * 310);
+  let width = Math.random() * 5;
+  let height = Math.random() * 50;
+  let duration = Math.random() * 0.5;
 
-menuIcon.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
+  e.classList.add('drop');
+  cloud.appendChild(e);
+  e.style.left = left + 'px';
+  e.style.width = 0.5 + width + 'px';
+  e.style.height = 0.5 + height + 'px';
+  e.style.animationDuration = 1 + duration + 's';
+
+  setTimeout(function(){
+    cloud.removeChild(e)
+  }, 2000)
+}
+
+setInterval(function(){
+  rain()
+},20);
